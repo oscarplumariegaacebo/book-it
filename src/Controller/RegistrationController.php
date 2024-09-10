1a2d0c0f2bea325dc791c95cd6e3bb7795432c71
Oscar Plumariega <oscarplumariegacebo@gmail.com>
2024-09-10 23:21:37 +0200

    Send confirmation email

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -47,7 +47,6 @@ class RegistrationController extends AbstractController
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
-
             // do anything else you need here, like send an email
 
             return $this->redirectToRoute('app_list');
