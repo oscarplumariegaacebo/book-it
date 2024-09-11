c96f53787a8247743ecbcb5473fb354b3346b3f7
Oscar Plumariega <oscarplumariegacebo@gmail.com>
2024-09-11 14:43:19 +0200

    CRUD all entities

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Company;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ListController extends AbstractController
 {
-    #[Route('/list', name: 'app_list')]
+    /*#[Route('/list', name: 'app_list')]
     public function index(): Response
     {
         return $this->render('list/index.html.twig', [
             'controller_name' => 'ListController',
+        ]);
+    }*/
+
+    #[Route('/list', name: 'app_list', methods: ['GET', 'POST'])]
+    public function connection(EntityManagerInterface $entityManager){
+        $list = $entityManager->getRepository(Company::class)->findAll();
+        //$list = $entityManager->getRepository(Company::class)->findBy(array('id' => 1));
+        return $this->render('list/index.html.twig', [
+            'controller_name' => 'Catálogo',
+            'companies' => $list
         ]);
     }
 }
---
+++ src/Form/CompanyType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Company;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CompanyType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('address')
+            ->add('region')
+            ->add('zip')
+            ->add('country')
+            ->add('city')
+            ->add('phoneNumber')
+            ->add('email')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Company::class,
+        ]);
+    }
+}
